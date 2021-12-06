8c86c3c4580aabc28de76b803a27cd545fdfc8d5
Omkar Pawar <71374293+OmkarPawar14@users.noreply.github.com>
2021-12-06 23:34:50 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,53 @@
 "use strict";
+const fadeIn_fadeOut_transition_speed = 300;
 
-document.querySelector(".showBot").addEventListener("click", function(){
-    document.querySelector(".Box").classList.remove("hidden");
-    document.querySelector(".showBot").classList.add("hidden");
-});
+function showBotFun(){
+    // Initialize Variables
+    const chatBotWindow =  document.getElementById("botWindow");
 
-document.querySelector(".hideBot").addEventListener("click", function(){
-    document.querySelector(".Box").classList.add("hidden");
+    //Main Logic to show chatBot
+    chatBotWindow.style.opacity=0;
+    chatBotWindow.style.transform=`translateY(10%)`;
+    document.querySelector(".container").classList.remove("hidden");
+
+    //Pausing the flow of the code for 0.3 secs
+    setTimeout(() => {
+        chatBotWindow.style.transform=`translateY(0%)`;
+        chatBotWindow.style.opacity=1;
+    }, fadeIn_fadeOut_transition_speed);
+
+    //Main Logic to hide Chat button
+    const showBot = document.getElementById("showBotbtn");
+    showBot.style.opacity=0;
+
+    //Pausing the flow of the code for 0.3 secs
+    setTimeout(() => {
+        document.querySelector(".showBot").classList.add("hidden");
+    }, fadeIn_fadeOut_transition_speed);
+
+}
+
+function hideBotFun(){
+    // Initialize Variables
+    const showBot = document.getElementById("showBotbtn");
+
+    //Main Logic to show Chat button
+    showBot.style.opacity=0;
+
+    //Pausing the flow of the code for 0.3 secs
     document.querySelector(".showBot").classList.remove("hidden");
-});
+    setTimeout(() => {
+            showBot.style.opacity=1;
+    }, fadeIn_fadeOut_transition_speed);
+
+    //Main Logic to hide chat bot
+    const chatBotWindow =  document.getElementById("botWindow");
+    chatBotWindow.style.transform=`translateY(11%)`;
+    chatBotWindow.style.opacity=0;
+    
+    //Pausing the flow of the code for 0.3 secs
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("hidden");
+    }, fadeIn_fadeOut_transition_speed);
+
+}
